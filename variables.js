3b66d59bd9753f04eb915612ee3b49c2a251a3d8
mahn20297-spec <mahn20297@gmail.com>
2026-01-18 08:14:32 -0800

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,11 @@
+let name = "Ayesha";        // String
+let age = 20;               // Number
+let isStudent = true;       // Boolean
+
+console.log("My name is " + name);
+console.log("I am " + age + " years old");
+console.log("Am I a student? " + isStudent);
+
+// Changing a variable
+age = 21;
+console.log("Next year, I will be " + age);
