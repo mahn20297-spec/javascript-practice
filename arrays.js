9fa994969dd5fcaa5826cee592dc1c6868865a21
mahn20297-spec <mahn20297@gmail.com>
2026-01-19 08:32:45 -0800

    Create arrays.js

---
+++ arrays.js
@@ -0,0 +1,15 @@
+let fruits = ["Apple", "Banana", "Mango"];
+console.log("Fruits:", fruits);
+
+// Access element
+console.log("First fruit:", fruits[0]);
+
+// Add new fruit
+fruits.push("Orange");
+console.log("After adding:", fruits);
+
+// Remove last fruit
+fruits.pop();
+console.log("After removing:", fruits);
+
+
