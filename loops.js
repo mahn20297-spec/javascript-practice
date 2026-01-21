fe848be429304430c6d75def539055af4e3c4aa2
mahn20297-spec <mahn20297@gmail.com>
2026-01-21 07:54:28 -0800

    Create loops.js
    Day 4: Loops

---
+++ loops.js
@@ -0,0 +1,12 @@
+
+// For loop
+for (let i = 1; i <= 5; i++) {
+  console.log("For loop count:", i);
+}
+
+// While loop
+let j = 1;
+while (j <= 5) {
+  console.log("While loop count:", j);
+  j++;
+}
