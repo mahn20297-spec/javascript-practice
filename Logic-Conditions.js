c411749b2297b028f9751867aedf5b5b4b354221
mahn20297-spec <mahn20297@gmail.com>
2026-02-23 12:05:22 -0800

    Create Logic-Conditions.js

---
+++ Logic-Conditions.js
@@ -0,0 +1,38 @@
+ // first program
+    let age = 15
+    if(age>18){
+        console.log("you can vote!")
+    }
+    else{
+        console.log("wait for it ")
+    }
+    console.log(age)
+// second program
+    let marks = 45
+    if(marks>=50){
+ console.log("pass")
+    }
+    else{
+                console.log("fail!")
+
+    }
+    // third program
+    let score = 80
+    if (score>=80){
+         console.log("grade A")
+    }
+    else if(score>60){
+                console.log("grade B")
+    }
+    else{
+    console.log("grade c")
+
+    }
+    // fourth program 
+    let score =75
+    if (score>=70 && score<=80){
+         console.log("Distinction")
+    }
+    else{
+            console.log("Normal")
+    }
