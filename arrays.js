dcebd279086ed0b9264e0ef572262b7988bf5b3a
mahn20297-spec <mahn20297@gmail.com>
2026-03-10 00:58:09 +0500

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -1,15 +1,35 @@
-let fruits = ["Apple", "Banana", "Mango"];
-console.log("Fruits:", fruits);
 
-// Access element
-console.log("First fruit:", fruits[0]);
+// Array intro and  methods 
 
-// Add new fruit
-fruits.push("Orange");
-console.log("After adding:", fruits);
+// 1 - programme 
+// push method  
+let bucket = [ "banana" , "apple" , "orange"]
 
-// Remove last fruit
-fruits.pop();
-console.log("After removing:", fruits);
+let r = bucket.push("mango")
+console.log(bucket , r)
+
+// unshift method 
+
+let r1 = bucket.unshift("orange")
+console.log(bucket , r , r1)
+
+
+// concat method 
+let a = [1,2,3,4,5]
+let b = [1,2,3,4,5]
+let c = a.concat(b)
+console.log(c)
+ let  str = c.toString()
+ console.log(c , str);
+
+
+//   shift and pop logic
+
+let colors = ["Red", "Green", "Blue", "Yellow"]
+let z = colors.shift("red")
+colors.pop()
+console.log( z ,colors )
+
+
 
 
